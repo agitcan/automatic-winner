97915d9b71607de5bb5d7076ee7bdfaf99b24dae
agitcan <103476906+agitcan@users.noreply.github.com>
2022-07-29 00:52:51 -0400

    Update karaboga.java

--- TEST/src/CanTest/karaboga.java
+++ TEST/src/CanTest/karaboga.java
@@ -4,7 +4,5 @@ public class karaboga {
     public static void main(String[] args) {
         System.out.println("karaboga");
 
-        System.out.println("final");
-        // create 2 maps and play with them
     }
 }
