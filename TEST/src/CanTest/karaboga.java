94eb72a126f5074fcdb734bfea287c61d1f1da21
Gull <gulkrbg@gmail.com>
2022-07-28 22:50:07 -0400

    1050

--- TEST/src/CanTest/karaboga.java
+++ TEST/src/CanTest/karaboga.java
@@ -5,5 +5,7 @@ public class karaboga {
     public static void main(String[] args) {
         System.out.println("karaboga");
         System.out.println("Agit");
+
+        String a = "can";
     }
 }
